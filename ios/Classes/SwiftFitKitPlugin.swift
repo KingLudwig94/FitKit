d86d8b454014fb0d7ab2938270caaffd7d710ada
Luca Cossu <luca@MacBook-Pro-di-Luca.local>
2020-02-18 17:43:04 +0100

    fix in case of locked screen

--- ios/Classes/SwiftFitKitPlugin.swift
+++ ios/Classes/SwiftFitKitPlugin.swift
@@ -1,10 +1,9 @@
 import Flutter
-import UIKit
 import HealthKit
+import UIKit
 
 public class SwiftFitKitPlugin: NSObject, FlutterPlugin {
-
-    private let TAG = "FitKit";
+    private let TAG = "FitKit"
 
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "fit_kit", binaryMessenger: registrar.messenger())
@@ -12,7 +11,7 @@ public class SwiftFitKitPlugin: NSObject, FlutterPlugin {
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
 
-    private var healthStore: HKHealthStore? = nil;
+    private var healthStore: HKHealthStore?
 
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         guard HKHealthStore.isHealthDataAvailable() else {
@@ -20,20 +19,20 @@ public class SwiftFitKitPlugin: NSObject, FlutterPlugin {
             return
         }
 
-        if (healthStore == nil) {
-            healthStore = HKHealthStore();
+        if healthStore == nil {
+            healthStore = HKHealthStore()
         }
 
         do {
-            if (call.method == "hasPermissions") {
+            if call.method == "hasPermissions" {
                 let request = try PermissionsRequest.fromCall(call: call)
                 hasPermissions(request: request, result: result)
-            } else if (call.method == "requestPermissions") {
+            } else if call.method == "requestPermissions" {
                 let request = try PermissionsRequest.fromCall(call: call)
                 requestPermissions(request: request, result: result)
-            } else if (call.method == "revokePermissions") {
+            } else if call.method == "revokePermissions" {
                 revokePermissions(result: result)
-            } else if (call.method == "read") {
+            } else if call.method == "read" {
                 let request = try ReadRequest.fromCall(call: call)
                 read(request: request, result: result)
             } else {
@@ -44,22 +43,21 @@ public class SwiftFitKitPlugin: NSObject, FlutterPlugin {
         }
     }
 
-
     /**
-    * On iOS you can only know if user has responded to request access screen.
-    * Not possible to tell if he has allowed access to read.
-    *
-    *   # getRequestStatusForAuthorization #
-    *   If "status == unnecessary" means if requestAuthorization will be called request access screen will not be shown.
-    *   So user has already responded to request access screen and kinda has permissions.
-    *
-    *   # authorizationStatus #
-    *   If "status == notDetermined" user has not responded to request access screen.
-    *   Once he responds no matter of the result status will be sharingDenied.
-    */
+     * On iOS you can only know if user has responded to request access screen.
+     * Not possible to tell if he has allowed access to read.
+     *
+     *   # getRequestStatusForAuthorization #
+     *   If "status == unnecessary" means if requestAuthorization will be called request access screen will not be shown.
+     *   So user has already responded to request access screen and kinda has permissions.
+     *
+     *   # authorizationStatus #
+     *   If "status == notDetermined" user has not responded to request access screen.
+     *   Once he responds no matter of the result status will be sharingDenied.
+     */
     private func hasPermissions(request: PermissionsRequest, result: @escaping FlutterResult) {
         if #available(iOS 12.0, *) {
-            healthStore!.getRequestStatusForAuthorization(toShare: [], read: Set(request.sampleTypes)) { (status, error) in
+            healthStore!.getRequestStatusForAuthorization(toShare: [], read: Set(request.sampleTypes)) { status, error in
                 guard error == nil else {
                     result(FlutterError(code: self.TAG, message: "hasPermissions", details: error))
                     return
@@ -74,17 +72,17 @@ public class SwiftFitKitPlugin: NSObject, FlutterPlugin {
             }
         } else {
             let authorized = request.sampleTypes.map {
-                        healthStore!.authorizationStatus(for: $0)
-                    }
-                    .allSatisfy {
-                        $0 != HKAuthorizationStatus.notDetermined
-                    }
+                healthStore!.authorizationStatus(for: $0)
+            }
+            .allSatisfy {
+                $0 != HKAuthorizationStatus.notDetermined
+            }
             result(authorized)
         }
     }
 
     private func requestPermissions(request: PermissionsRequest, result: @escaping FlutterResult) {
-        requestAuthorization(sampleTypes: request.sampleTypes) { success, error in
+        requestAuthorization(sampleTypes: request.sampleTypes) { success, _ in
             guard success else {
                 result(false)
                 return
@@ -95,8 +93,8 @@ public class SwiftFitKitPlugin: NSObject, FlutterPlugin {
     }
 
     /**
-    * Not supported by HealthKit.
-    */
+     * Not supported by HealthKit.
+     */
     private func revokePermissions(result: @escaping FlutterResult) {
         result(nil)
     }
@@ -112,8 +110,8 @@ public class SwiftFitKitPlugin: NSObject, FlutterPlugin {
         }
     }
 
-    private func requestAuthorization(sampleTypes: Array<HKSampleType>, completion: @escaping (Bool, FlutterError?) -> Void) {
-        healthStore!.requestAuthorization(toShare: nil, read: Set(sampleTypes)) { (success, error) in
+    private func requestAuthorization(sampleTypes: [HKSampleType], completion: @escaping (Bool, FlutterError?) -> Void) {
+        healthStore!.requestAuthorization(toShare: nil, read: Set(sampleTypes)) { success, error in
             guard success else {
                 completion(false, FlutterError(code: self.TAG, message: "Error \(error?.localizedDescription ?? "empty")", details: nil))
                 return
@@ -124,38 +122,42 @@ public class SwiftFitKitPlugin: NSObject, FlutterPlugin {
     }
 
     private func readSample(request: ReadRequest, result: @escaping FlutterResult) {
-        print("readSample: \(request.type)")
+        // if UIApplication.shared.isProtectedDataAvailable {
+        if(UIScreen.main.brightness != 0.0){
+            print("readSample: \(request.type)")
 
-        let predicate = HKQuery.predicateForSamples(withStart: request.dateFrom, end: request.dateTo, options: .strictStartDate)
-        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: request.limit == nil)
+            let predicate = HKQuery.predicateForSamples(withStart: request.dateFrom, end: request.dateTo, options: .strictStartDate)
+            let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: request.limit == nil)
+            let query = HKSampleQuery(sampleType: request.sampleType, predicate: predicate, limit: request.limit ?? HKObjectQueryNoLimit, sortDescriptors: [sortDescriptor]) {
+                _, samplesOrNil, error in
 
-        let query = HKSampleQuery(sampleType: request.sampleType, predicate: predicate, limit: request.limit ?? HKObjectQueryNoLimit, sortDescriptors: [sortDescriptor]) {
-            _, samplesOrNil, error in
+                guard var samples = samplesOrNil else {
+                    result(FlutterError(code: self.TAG, message: "Results are null", details: error))
+                    return
+                }
 
-            guard var samples = samplesOrNil else {
-                result(FlutterError(code: self.TAG, message: "Results are null", details: error))
-                return
+                if request.limit != nil {
+                    // if limit is used sort back to ascending
+                    samples = samples.sorted(by: { $0.startDate.compare($1.startDate) == .orderedAscending })
+                }
+
+                print(samples)
+                result(samples.map { sample -> NSDictionary in
+                    [
+                        "value": self.readValue(sample: sample, unit: request.unit),
+                        "date_from": Int(sample.startDate.timeIntervalSince1970 * 1000),
+                        "date_to": Int(sample.endDate.timeIntervalSince1970 * 1000),
+                        "source": self.readSource(sample: sample),
+                        "user_entered": sample.metadata?[HKMetadataKeyWasUserEntered] as? Bool == true,
+                        "type": HKSampleType.toDartType(type: sample.sampleType),
+                        "unit": request.unit.unitString,
+                    ]
+                })
             }
-
-            if (request.limit != nil) {
-                // if limit is used sort back to ascending
-                samples = samples.sorted(by: { $0.startDate.compare($1.startDate) == .orderedAscending })
-            }
-
-            print(samples)
-            result(samples.map { sample -> NSDictionary in
-                [
-                    "value": self.readValue(sample: sample, unit: request.unit),
-                    "date_from": Int(sample.startDate.timeIntervalSince1970 * 1000),
-                    "date_to": Int(sample.endDate.timeIntervalSince1970 * 1000),
-                    "source": self.readSource(sample: sample),
-                    "user_entered": sample.metadata?[HKMetadataKeyWasUserEntered] as? Bool == true,
-                    "type": HKSampleType.toDartType(type: sample.sampleType),
-                    "unit": request.unit.unitString
-                ]
-            })
-        }
-        healthStore!.execute(query)
+            healthStore!.execute(query)
+         } else {
+             result([String: Any]())
+         }
     }
 
     private func readValue(sample: HKSample, unit: HKUnit) -> Any {
@@ -170,9 +172,9 @@ public class SwiftFitKitPlugin: NSObject, FlutterPlugin {
 
     private func readSource(sample: HKSample) -> String {
         if #available(iOS 9, *) {
-            return sample.sourceRevision.source.name;
+            return sample.sourceRevision.source.name
         }
 
-        return sample.source.name;
+        return sample.source.name
     }
 }
